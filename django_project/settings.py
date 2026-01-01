64265312934f06ed7fdac550e97da4a9fb521102
ttanvirr <tanvir.artisan@gmail.com>
2026-01-01 10:38:20 +0600

    Edited allowed host for testing purpose

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
