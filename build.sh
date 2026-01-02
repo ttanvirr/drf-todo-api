f89708b51e7707d30a77a272446d3c69dd5b6230
ttanvirr <tanvir.artisan@gmail.com>
2026-01-02 07:56:05 +0600

    Updated build.sh to create superuser

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser if the environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --no-input
+fi
